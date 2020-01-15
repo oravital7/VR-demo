c51a0a2c74a96e5bb47e4e44067e16cf27ced1b8
Or Avital <oravital7@gmail.com>
2020-01-15 19:11:38 +0200

    Minors style

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
   
-/*jshint esversion: 6 */
-/*jshint browser: true */
-/*jshint devel: true */
+const sphere = document.querySelector('a-sphere');
+const plane = document.querySelector('a-plane');
 
-const $ = (query) => document.querySelector(query);
-
-const sphere = $('a-sphere');
-const plane = $('a-plane');
-
-const shiftDegrees = (value) => (value + 1) % 360;
+const shiftDegrees = (value) => (value + 0.5) % 360;
 
 let degrees = 0;
 
 const animate = () => {
   degrees = shiftDegrees(degrees);
-  const color = `hsl(${degrees}, 100%, 50%)`;
+  const color = `hsl(${degrees + 0.5}, 100%, 50%)`;
   const variation = Math.sin(Date.now() / 1000);
   const position = `0 ${1.5 + variation} -2`;
   const rotation = `-90 0 ${degrees}`;
@@ -22,7 +16,7 @@ const animate = () => {
   sphere.setAttribute('color', color);
   sphere.setAttribute('position', position);
 
-  plane.setAttribute('color', color);
+  // plane.setAttribute('color', color);
   plane.setAttribute('rotation', rotation);
 
   requestAnimationFrame(animate);
